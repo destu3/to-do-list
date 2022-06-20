c6a641f69dd09e29b36d7b7c05d7934f65a481d8
destu3 <obose03@gmail.com>
2022-06-20 11:42:42 +0100

    init commit, file setup

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+import { resolve } from "path";
+
+export const entry = "./src/index.js";
+export const output = {
+  filename: "bundle.js",
+  path: resolve(__dirname, "dist"),
+};
+export const module = {
+  rules: [
+    {
+      test: /\.css$/i,
+      use: ["style-loader", "css-loader"],
+    },
+  ],
+};
