591e61940d13b985f8c59856353c6c0c5d27a3f0
destu3 <obose03@gmail.com>
2022-06-25 13:28:45 +0100

    pfp is now bundled with webpack

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import "../src/styles/dashboard.css";
 import { openNewTaskModal, closeNewTaskModal, defaultGenerateTasks } from "./DOM.js";
-import { DEFAULT_PROJECTS } from "./projects";
 import { save, newTask } from "./tasks";
+import Icon from "./assets/mock-pfp.jpg";
 
 // selecting main DOM elements
 const openModal = document.querySelector("#new-task-btn");
 const closeModal = document.querySelector(".close-btn");
 const createTask = document.querySelector(".submit-form-button");
+const userPfp = document.querySelector(".profile-picture");
 
 // content that is generated when the window loads
 window.addEventListener("DOMContentLoaded", defaultGenerateTasks);
+window.addEventListener("DOMContentLoaded", () => {
+  userPfp.src = Icon;
+});
+
 //task modal handlers
 openModal.addEventListener("click", openNewTaskModal);
 closeModal.addEventListener("click", closeNewTaskModal);
